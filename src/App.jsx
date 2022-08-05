07d7b05853ac14c055fd5bad7fbfa3dd0c0040be
Grant Greer <viridiang93@gmail.com>
2022-08-05 15:16:53 -0700

    added axios and prettier/eslint config

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  return <div className="h-screen bg-[url('./animalgc.jpg')]">hello world</div>;
+}
+
+export default App;
