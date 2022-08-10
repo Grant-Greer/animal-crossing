ba8e016427aef0d5b876af283bde58a5247196f2
Grant Greer <viridiang93@gmail.com>
2022-08-09 21:50:48 -0700

    async functions for retrieving api data

---
+++ src/services/api-calls.js
@@ -0,0 +1,15 @@
+const baseUrl = 'http://acnhapi.com/v1a'
+
+const getFossils = async () => {
+    const response = await fetch(`${baseUrl}/fossils`)
+    const data = await response.json()
+    return data
+}
+
+const getFish = async () => {
+    const response = await fetch(`${baseUrl}/fish`)
+    const data = await response.json()
+    return data
+}
+
+export default { getFish, getFossils }
